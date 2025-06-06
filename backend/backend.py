61e845101ff03a4cbb6759b3a82d308274b09237
Riddhima Chauhan <149195451+riddhimachauhan@users.noreply.github.com>
2025-06-06 16:11:42 +0530

    Update backend.py

--- backend/backend.py
+++ backend/backend.py
@@ -506,8 +506,8 @@ def get_tracks_by_release_date():
 def home():
     return "Backend is running!"
 
+import os
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-    app.run(port=5000, debug=True)
